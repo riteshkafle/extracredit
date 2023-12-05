8f0c7d5285550a5bdc3b349ed9fdafdce3b80fd5
Ritesh Kafle <85400895+riteshkafle@users.noreply.github.com>
2023-12-04 23:39:50 -0500

    Start Main Program
    Set up main program and input prompt.

--- extraproject.s
+++ extraproject.s
@@ -1,3 +1,9 @@
 .data
 prompt:          .asciiz "Enter an integer greater than 20): "
 error_message:   .asciiz "Number Illegal !\n"
+
+.text
+.globl main
+main:
+    # Main program code will be here
+
